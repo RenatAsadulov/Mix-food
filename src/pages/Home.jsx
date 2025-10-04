e84b2ba4e7ebe2645e3aad971e5565690ad0118c
Renat Asadulov <r.asadulov@joinposter.com>
2025-10-04 13:21:27 +0300

    add log to detect cicd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import NewsSection from "@/sections/NewsSection";
 import Contact from "@/sections/Contact";
 
 export default function Home() {
+  console.log("hello from ci cd", new Date());
+
   return (
     <>
       <Hero />
